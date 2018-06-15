c396557b5b6b28ab7c7193ad2d347f0292d48211
Dario Pilacuan <dario.pilacuan@epn.edu.ec>
2018-06-14 20:17:40 -0500

    ingreso de numeros
    creacion de funciones de suma resta multiplicacion y divicion

--- js/app.js
+++ js/app.js
@@ -1,198 +1,366 @@
 /*objeto calculadora*/
 var calculadora = {
-	/*metodos*/
+		//variables
+		display: document.getElementById("display"),
+		uno: document.getElementById('1'),
+		dos: document.getElementById('2'),
+		tres: document.getElementById('3'),
+		cuatro: document.getElementById('4'),
+		cinco: document.getElementById('5'),
+		seis: document.getElementById('6'),
+		siete: document.getElementById('7'),
+		ocho: document.getElementById('8'),
+		nueve: document.getElementById('9'),
+		cero: document.getElementById('0'),
+		mas: document.getElementById('mas'),
+		menos: document.getElementById('menos'),
+		por: document.getElementById('por'),
+		dividido: document.getElementById('dividido'),
+		raiz: document.getElementById('raiz'),
+		sign: document.getElementById('sign'),
+		on: document.getElementById('on'),
+		igual: document.getElementById("igual"),
+		signopunto: document.getElementById("punto"),
+		operandoa: 0,
+		operandob: 0,
+		operacion: 0,
+	
 
-	presionarteclaon: function () {
-		document.getElementById('on').style = "width:21% ;height: 58.91px"
-	},
-	retornarteclaon: function () {
-		document.getElementById('on').style = "width:22% ;height: 62.91px"
-	},
-	presionarteclasign: function () {
-		document.getElementById('sign').style = "width:21% ;height: 58.91px"
-	},
-	retornarteclasing: function () {
-		document.getElementById('sign').style = "width:22% ;height: 62.91px"
-	},
-	presionarteclaraiz: function () {
-		document.getElementById('raiz').style = "width:21% ;height: 58.91px"
-	},
-	retornarteclaraiz: function () {
-		document.getElementById('raiz').style = "width:22% ;height: 62.91px"
-	},
-	presionartecladivido: function () {
 
-		document.getElementById('dividido').style = "width:21% ;height: 58.91px"
+
+		//metodo para mostrar numeros en pantalla
+
+		numero1: function () {
+		
+
+				calculadora.display.textContent = calculadora.display.textContent + "1"
+			
+		
+		
+
+		},
+		numero2: function () {
+
+			calculadora.display.textContent = calculadora.display.textContent + "2"
+		},
+		numero3: function () {
+
+			calculadora.display.textContent = calculadora.display.textContent + "3"
+		},
+		numero4: function () {
+
+			calculadora.display.textContent = calculadora.display.textContent + "4"
+		},
+		numero5: function () {
+
+			calculadora.display.textContent = calculadora.display.textContent + "5"
+		},
+		numero6: function () {
+
+			calculadora.display.textContent = calculadora.display.textContent + "6"
+		},
+		numero7: function () {
+
+			calculadora.display.textContent = calculadora.display.textContent + "7"
+		},
+		numero8: function () {
+
+			calculadora.display.textContent = calculadora.display.textContent + "8"
+		},
+
+		numero9: function () {
+
+			calculadora.display.textContent = calculadora.display.textContent + "9"
+		},
+
+		numero0: function () {
+
+			calculadora.display.textContent = calculadora.display.textContent + "0"
+		},
+
+
+
+
+
+		//metodos pra presionar las teclas
+
+
+
+		presionarteclaon: function () {
+			calculadora.on.style = "width:21% ;height: 58.91px"
+			calculadora.limpiar();
+
+		},
+		retornarteclaon: function () {
+			calculadora.on.style = "width:22% ;height: 62.91px"
+		},
+		presionarteclasign: function () {
+			calculadora.sign.style = "width:21% ;height: 58.91px"
+			calculadora.sigononegativo();
+		},
+		retornarteclasing: function () {
+			calculadora.sign.style = "width:22% ;height: 62.91px"
+		},
+		presionarteclaraiz: function () {
+			document.getElementById('raiz').style = "width:21% ;height: 58.91px"
+		},
+		retornarteclaraiz: function () {
+			document.getElementById('raiz').style = "width:22% ;height: 62.91px"
+		},
+		presionartecladivido: function () {
+			document.getElementById('dividido').style = "width:21% ;height: 58.91px"
+			calculadora.dividir();
+		},
+		retornartecladividido: function () {
+			document.getElementById('dividido').style = "width:22% ;height: 62.91px"
+		},
+		presionartecla7: function () {
+			document.getElementById('7').style = "width:21% ;height: 58.91px";
+	
+			calculadora.numero7();
+		},
+		retornartecla7: function () {
+			document.getElementById('7').style = "width:22% ;height: 62.91px"
+	
+		},
+		presionartecla8: function () {
+			document.getElementById('8').style = "width:21% ;height: 58.91px"
+			calculadora.numero8();
+		},
+		retornartecla8: function () {
+			document.getElementById('8').style = "width:22% ;height: 62.91px"
+		},
+		presionartecla9: function () {
+			document.getElementById('9').style = "width:21% ;height: 58.91px"
+			calculadora.numero9();
+		},
+		retornartecla9: function () {
+			document.getElementById('9').style = "width:22% ;height: 62.91px"
+		},
+
+		presionarteclaX: function () {
+			document.getElementById('por').style = "width:21% ;height: 58.91px"
+			calculadora.multiplicar();
+		},
+		retornarteclaX: function () {
+			document.getElementById('por').style = "width:22% ;height: 62.91px"
+		},
+		presionartecla4: function () {
+			document.getElementById('4').style = "width:21% ;height: 58.91px"
+			
+			calculadora.numero4();
+		},
+		retornartecla4: function () {
+			document.getElementById('4').style = "width:22% ;height: 62.91px"
+		},
+		presionartecla5: function () {
+			document.getElementById('5').style = "width:21% ;height: 58.91px"
+			calculadora.numero5();
+		},
+		retornartecla5: function () {
+			document.getElementById('5').style = "width:22% ;height: 62.91px"
+		},
+		presionartecla6: function () {
+			document.getElementById('6').style = "width:21% ;height: 58.91px"
+			calculadora.numero6();
+		},
+		retornartecla6: function () {
+			document.getElementById('6').style = "width:22% ;height: 62.91px"
+		},
+		presionarteclamenos: function () {
+			document.getElementById('menos').style = "width:21% ;height: 58.91px"
+			calculadora.restar();
+		},
+		retornarteclamenos: function () {
+			document.getElementById('menos').style = "width:22% ;height: 62.91px"
+		},
+		presionartecla1: function () {
+			calculadora.uno.style = "width:26% ;height: 58.91px"
+			calculadora.numero1();
+		},
+		retornartecla1: function () {
+			calculadora.uno.style = "width:28% ;height: 62.91px"
+		},
+		presionarteclamas: function () {
+			document.getElementById('mas').style = "  width: 88%; height: 98%; margin-left: 8px"
+			calculadora.sumar();
+		},
+		retornarteclamas: function () {
+			document.getElementById('mas').style = "  width: 90%; height: 100%; margin-left: 10px"
+		},
+		presionartecla0: function () {
+			document.getElementById('0').style = "width:26% ;height: 58.91px"
+			calculadora.numero0();
+		},
+		retornartecla0: function () {
+			document.getElementById('0').style = "width:28% ;height: 62.91px"
+		},
+		presionartecla2: function () {
+			calculadora.dos.style = "width:26% ;height: 58.91px"
+		
+			calculadora.numero2();
+		},
+		retornartecla2: function () {
+			calculadora.dos.style = "width:28% ;height: 62.91px"
+		},
+		presionartecla3: function () {
+			document.getElementById('3').style = "width:26% ;height: 58.91px"
+			calculadora.numero3();
+
+		},
+		retornartecla3: function () {
+			document.getElementById('3').style = "width:29% ;height: 62.91px"
+		},
+		presionarteclapunto: function () {
+			document.getElementById('punto').style = "width:26% ;height: 58.91px"
+			calculadora.punto();
+		},
+		retornarteclapunto: function () {
+			document.getElementById('punto').style = "width:28% ;height: 62.91px"
+		},
+		presionarteclaigual: function () {
+			document.getElementById('igual').style = "width:26% ;height: 58.91px"
+			calculadora.resultado();
+		},
+		retornarteclaigual: function () {
+			document.getElementById('igual').style = "width:29% ;height: 62.91px"
+
+		},
+
+		//calculos 
+	divcionparacero: function(){
+		calculadora.operandoa=calculadora.display.textContent/0
+		return false;
 	},
-	retornartecladividido: function () {
 
-		document.getElementById('dividido').style = "width:22% ;height: 62.91px"
-	},
-	presionartecla7: function () {
-		document.getElementById('7').style = "width:21% ;height: 58.91px"
-	},
-	retornartecla7: function () {
-		document.getElementById('7').style = "width:22% ;height: 62.91px"
-	},
-	presionartecla8: function () {
-		document.getElementById('8').style = "width:21% ;height: 58.91px"
-	},
-	retornartecla8: function () {
-		document.getElementById('8').style = "width:22% ;height: 62.91px"
-	},
-	presionartecla9: function () {
+		punto: function () {
+			calculadora.display.textContent = calculadora.display.textContent + "."
 
-		document.getElementById('9').style = "width:21% ;height: 58.91px"
-	},
-	retornartecla9: function () {
+		},
+		sigononegativo: function () {
 
-		document.getElementById('9').style = "width:22% ;height: 62.91px"
-	},
+			var negativos = calculadora.display.textContent = "-" + calculadora.display.textContent
 
-	presionarteclaX: function () {
+		},
 
-		document.getElementById('por').style = "width:21% ;height: 58.91px"
-	},
-	retornarteclaX: function () {
+		sumar: function () {
+			calculadora.operandoa = calculadora.display.textContent;
+			calculadora.operacion = "+";
+			calculadora.limpiar();
 
-		document.getElementById('por').style = "width:22% ;height: 62.91px"
-	},
-	presionartecla4: function () {
 
-		document.getElementById('4').style = "width:21% ;height: 58.91px"
-	},
-	retornartecla4: function () {
 
-		document.getElementById('4').style = "width:22% ;height: 62.91px"
-	},
-	presionartecla5: function () {
+		},
+		restar: function () {
+			calculadora.operandoa = calculadora.display.textContent;
+			calculadora.operacion = "-";
+			calculadora.limpiar();
 
-		document.getElementById('5').style = "width:21% ;height: 58.91px"
-	},
-	retornartecla5: function () {
 
-		document.getElementById('5').style = "width:22% ;height: 62.91px"
-	},
-	presionartecla6: function () {
 
-		document.getElementById('6').style = "width:21% ;height: 58.91px"
-	},
-	retornartecla6: function () {
+		},
+		multiplicar: function () {
+			calculadora.operandoa = calculadora.display.textContent;
+			calculadora.operacion = "*";
+			calculadora.limpiar();
 
-		document.getElementById('6').style = "width:22% ;height: 62.91px"
-	},
-	presionarteclamenos: function () {
 
-		document.getElementById('menos').style = "width:21% ;height: 58.91px"
-	},
-	retornarteclamenos: function () {
 
-		document.getElementById('menos').style = "width:22% ;height: 62.91px"
-	},
+		},
+		dividir: function () {
+			calculadora.operandoa = calculadora.display.textContent;
+			calculadora.operacion = "/";
+			calculadora.limpiar();
 
-	presionartecla1: function () {
 
-		document.getElementById('1').style = "width:26% ;height: 58.91px"
-	},
-	retornartecla1: function () {
 
-		document.getElementById('1').style = "width:28% ;height: 62.91px"
-	},
-	presionarteclamas: function () {
+		},
 
-		document.getElementById('mas').style = "  width: 88%; height: 98%; margin-left: 8px"
-	},
-	retornarteclamas: function () {
+		//limpiar  pantalla
 
-		document.getElementById('mas').style = "  width: 90%; height: 100%; margin-left: 10px"
-	},
-	presionartecla0: function () {
+		limpiar: function () {
 
-		document.getElementById('0').style = "width:26% ;height: 58.91px"
-	},
-	retornartecla0: function () {
+			calculadora.display.textContent = "0" || " ";
+		},
 
-		document.getElementById('0').style = "width:28% ;height: 62.91px"
-	},
-	presionartecla2: function () {
+		resultado: function () {
+			calculadora.operandob = calculadora.operandob + calculadora.display.textContent;
+			calculadora.resolver();
 
-		document.getElementById('2').style = "width:26% ;height: 58.91px"
-	},
-	retornartecla2: function () {
 
-		document.getElementById('2').style = "width:28% ;height: 62.91px"
-	},
+		},
 
-	presionartecla3: function () {
+		resetear: function () {
 
-		document.getElementById('3').style = "width:26% ;height: 58.91px"
-	},
-	retornartecla3: function () {
+			calculadora.display.textContent = "0";
+			calculadora.operandoa = "0";
+			calculadora.operandob = "0";
+			calculadora.operacion = "";
+		},
+		resolver: function () {
 
-		document.getElementById('3').style = "width:29% ;height: 62.91px"
-	},
-	presionarteclapunto: function () {
+			var res = 0;
+			switch (calculadora.operacion) {
+				case "+":
+					res = parseFloat(calculadora.operandoa) + parseFloat(calculadora.operandob);
+					break;
+				case "-":
+					res = parseFloat(calculadora.operandoa) - parseFloat(calculadora.operandob);
 
-		document.getElementById('punto').style = "width:26% ;height: 58.91px"
-	},
-	retornarteclapunto: function () {
+					break;
+				case "*":
+					res = parseFloat(calculadora.operandoa) * parseFloat(calculadora.operandob);
 
-		document.getElementById('punto').style = "width:28% ;height: 62.91px"
-	},
+					break;
+				case "/":
+					res = parseFloat(calculadora.operandoa) / parseFloat(calculadora.operandob);
 
-	presionarteclaigual: function () {
+			}
+			calculadora.resetear();
+			calculadora.display.textContent = res.toFixed(2);
+		},
+		//carga de metodos
+		inicializacion: function () {
+			calculadora.on.onmousedown = calculadora.presionarteclaon;
+			calculadora.on.onclick = calculadora.retornarteclaon;
+			calculadora.sign.onmousedown = calculadora.presionarteclasign;
+			calculadora.sign.onclick = calculadora.retornarteclasing;
+			calculadora.raiz.onmousedown = calculadora.presionarteclaraiz;
+			calculadora.raiz.onclick = calculadora.retornarteclaraiz;
+			calculadora.dividido.onmousedown = calculadora.presionartecladivido;
+			calculadora.dividido.onclick = calculadora.retornartecladividido;
+			calculadora.siete.onmousedown = calculadora.presionartecla7
+			calculadora.siete.onclick = calculadora.retornartecla7
+			calculadora.ocho.onmousedown = calculadora.presionartecla8;
+			calculadora.ocho.onclick = calculadora.retornartecla8;
+			calculadora.nueve.onmousedown = calculadora.presionartecla9;
+			calculadora.nueve.onclick = calculadora.retornartecla9;
+			calculadora.por.onmousedown = calculadora.presionarteclaX;
+			calculadora.por.onclick = calculadora.retornarteclaX;
+			calculadora.cuatro.onmousedown = calculadora.presionartecla4;
+			calculadora.cuatro.onclick = calculadora.retornartecla4
+			calculadora.cinco.onmousedown = calculadora.presionartecla5;
+			calculadora.cinco.onclick = calculadora.retornartecla5;
+			calculadora.seis.onmousedown = calculadora.presionartecla6;
+			calculadora.seis.onclick = calculadora.retornartecla6;
+			calculadora.menos.onmousedown = calculadora.presionarteclamenos;
+			calculadora.menos.onclick = calculadora.retornarteclamenos;
+			calculadora.mas.onmousedown = calculadora.presionarteclamas;
+			calculadora.mas.onclick = calculadora.retornarteclamas;
+			calculadora.cero.onmousedown = calculadora.presionartecla0;
+			calculadora.cero.onclick = calculadora.retornartecla0;
+			calculadora.uno.onmousedown = calculadora.presionartecla1;
+			calculadora.uno.onclick = calculadora.retornartecla1;
+			calculadora.dos.onmousedown = calculadora.presionartecla2;
+			calculadora.dos.onclick = calculadora.retornartecla2;
+			calculadora.tres.onmousedown = calculadora.presionartecla3;
+			calculadora.tres.onclick = calculadora.retornartecla3;
+			calculadora.signopunto.onmousedown = calculadora.presionarteclapunto;
+			calculadora.signopunto.onclick = calculadora.retornarteclapunto;
+			calculadora.igual.onmousedown = calculadora.presionarteclaigual;
+			calculadora.igual.onclick = calculadora.retornarteclaigual;
 
-		document.getElementById('igual').style = "width:26% ;height: 58.91px"
-	},
-	retornarteclaigual: function () {
-
-		document.getElementById('igual').style = "width:28% ;height: 62.91px"
-	},
-
-	inicializacion: function () {
-
-		document.getElementById('on').onmousedown = calculadora.presionarteclaon;
-		document.getElementById('on').onclick = calculadora.retornarteclaon;
-		document.getElementById('sign').onmousedown = calculadora.presionarteclasign;
-		document.getElementById('sign').onclick = calculadora.retornarteclasing;
-		document.getElementById('raiz').onmousedown = calculadora.presionarteclaraiz;
-		document.getElementById('raiz').onclick = calculadora.retornarteclaraiz;
-		document.getElementById('dividido').onmousedown = calculadora.presionartecladivido;
-		document.getElementById('dividido').onclick = calculadora.retornartecladividido;
-		document.getElementById('7').onmousedown = calculadora.presionartecla7;
-		document.getElementById('7').onclick = calculadora.retornartecla7;
-		document.getElementById('8').onmousedown = calculadora.presionartecla8;
-		document.getElementById('8').onclick = calculadora.retornartecla8;
-		document.getElementById('9').onmousedown = calculadora.presionartecla9;
-		document.getElementById('9').onclick = calculadora.retornartecla9;
-		document.getElementById('por').onmousedown = calculadora.presionarteclaX;
-		document.getElementById('por').onclick = calculadora.retornarteclaX;
-		document.getElementById('4').onmousedown = calculadora.presionartecla4;
-		document.getElementById('4').onclick = calculadora.retornartecla4;
-		document.getElementById('5').onmousedown = calculadora.presionartecla5;
-		document.getElementById('5').onclick = calculadora.retornartecla5;
-		document.getElementById('6').onmousedown = calculadora.presionartecla6;
-		document.getElementById('6').onclick = calculadora.retornartecla6;
-		document.getElementById('menos').onmousedown = calculadora.presionarteclamenos;
-		document.getElementById('menos').onclick = calculadora.retornarteclamenos;
-		document.getElementById('mas').onmousedown = calculadora.presionarteclamas;
-		document.getElementById('mas').onclick = calculadora.retornarteclamas;
-		document.getElementById('0').onmousedown = calculadora.presionartecla0;
-		document.getElementById('0').onclick = calculadora.retornartecla0;
-		document.getElementById('1').onmousedown = calculadora.presionartecla1;
-		document.getElementById('1').onclick = calculadora.retornartecla1;
-		document.getElementById('2').onmousedown = calculadora.presionartecla2;
-		document.getElementById('2').onclick = calculadora.retornartecla2;
-		document.getElementById('3').onmousedown = calculadora.presionartecla3;
-		document.getElementById('3').onclick = calculadora.retornartecla3;
-		document.getElementById('punto').onmousedown = calculadora.presionarteclapunto;
-		document.getElementById('punto').onclick = calculadora.retornarteclapunto;
-		document.getElementById('igual').onmousedown = calculadora.presionarteclaigual;
-		document.getElementById('igual').onclick = calculadora.retornarteclaigual;
-
+		},
 	}
-
-}
-
-
+	//llamada de objeto calculadora
 calculadora.inicializacion();
